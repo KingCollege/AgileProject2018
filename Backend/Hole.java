7d1c290569daff679931e59ffc7727960752b47c
tao lin <alexlint1998@gmail.com>
2018-11-12 12:23:52 +0000

    upload

---
+++ Backend/Hole.java
@@ -0,0 +1,34 @@
+package Backend;
+
+public class Hole {
+    private int balls_num;
+    private boolean hole_side;
+    private int hindex;
+    private boolean tuz;
+    public  Hole(int num,boolean side,int index){//if it's true then for playerside//
+        balls_num = num;
+        hole_side = side;
+        hindex = index;
+        tuz = false;
+    }
+    boolean markAstuz(){
+        tuz = true;
+        return tuz;
+    }
+    boolean checkTuz(){
+        return tuz;
+    }
+    int getNum(){
+        return balls_num;
+    }
+    int getIndex(){
+        return hindex;
+    }
+    boolean getSide(){
+        return hole_side;
+    }
+    void changeNum(int Cnum){
+        balls_num = Cnum;
+    }
+
+}
