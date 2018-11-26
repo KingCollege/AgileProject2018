9710463bc2f1cb5f5047cebcb2199c92adab6e1b
Mandu Shi <mandushi.18@gmail.com>
2018-11-26 22:53:08 +0000

    Update backend with latest frontend code

--- Front-End/src/main/java/playTheGame.java
+++ Front-End/src/main/java/playTheGame.java
@@ -9,15 +9,15 @@ public class playTheGame
 {
     public static Board board = new Board();
     private static boolean pTurn = true;
-    
+
     public static boolean getPlayerTurn(){
         return pTurn;
     }
-    
+
     public static void setPlayerTurn(boolean turn){
         pTurn = turn;
     }
-    
+
     public static String computerPlay(){
         Random r = new Random();
         //computer holes start after 9
@@ -28,7 +28,7 @@ public class playTheGame
                 continue;
             }
             if(pTurn)
-                return "Log: " + (chooseHole - 9);
+                return "Log: " + (chooseHole - 8 );
         }
         return "Went wrong!";
     }
